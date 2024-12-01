dd63e621c41e3d2efc812153cb44267f8403698c
tryan17 <tryapriyana@gmail.com>
2024-12-02 00:05:10 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Toggle class active untuk shopping cart
+const shoppingCart = document.querySelector(".shopping-cart");
+document.querySelector("#shopping-cart-button").onclick = (e) => {
+  shoppingCart.classList.toggle("active");
+  e.preventDefault();
+};
+
+// Klik di luar elemen
+const sc = document.querySelector("#shopping-cart-button");
+
+document.addEventListener("click", function (e) {
+  if (!sc.contains(e.target) && !shoppingCart.contains(e.target)) {
+    shoppingCart.classList.remove("active");
+  }
+});
