cf340f89f6e9ba0507f03a8c2e50d2bdce26dec5
tryan17 <tryapriyana@gmail.com>
2024-12-02 07:35:26 +0700

    preload, favicon

--- js/script.js
+++ js/script.js
@@ -13,3 +13,13 @@ document.addEventListener("click", function (e) {
     shoppingCart.classList.remove("active");
   }
 });
+
+// prefetch gambar
+const images = ["americano-cold.jpg", "americano-hot.jpg", "cappucino-hot.jpg", "caramel-machiato.jpg", "caramel-milk.jpg", "chocolate-hot.jpg", "espresso-1-shoot.jpg", "green-tea-hot.jpg", "gula-aren.jpg", "hazelnut-milk.jpg", "japanese.jpg", "kopi-tubruk.jpg", "long-black-cold.jpg", "long-black-hot.jpg", "matcha-milk.jpg", "mochacino.jpg", "red-velvet-hot.jpg", "redvelvet-milk.jpg", "v60.jpg", "vanilla-latte-cold.jpg", "vanilla-milk.jpg", "vietnam-drip.jpg" ]; // Daftar gambar
+images.forEach(img => {
+  const link = document.createElement("link");
+  link.rel = "prefetch";
+  link.as = "image";
+  link.href = `img/products/${img}`;
+  document.head.appendChild(link);
+});
